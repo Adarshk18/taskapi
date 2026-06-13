ee1968a9286eceae686e1d61892cbe5c6e7496d1
Adarshk18 <adarshkohli183@gmail.com>
2026-06-13 14:07:07 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import tasks
 from app.database import Base, engine
 
-Base.meatdata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title="Task Manager API")
